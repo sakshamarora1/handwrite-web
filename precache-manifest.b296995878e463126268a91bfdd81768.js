552296d16279861b53ab50be9d6965a3de3ef447
sakshamarora1 <sakshamarora1001@gmail.com>
2021-09-06 14:55:15 +0530

    Updates

---
+++ precache-manifest.b296995878e463126268a91bfdd81768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0aaae6acd27373c0cac",
+    "url": "/handwrite-web/static/css/main.87b2c8de.chunk.css"
+  },
+  {
+    "revision": "e0aaae6acd27373c0cac",
+    "url": "/handwrite-web/static/js/main.a9b1b8e9.chunk.js"
+  },
+  {
+    "revision": "eed8945052c3d544c1f9",
+    "url": "/handwrite-web/static/js/runtime~main.577dc6c9.js"
+  },
+  {
+    "revision": "c90c083ec67a31d15529",
+    "url": "/handwrite-web/static/css/2.42d25497.chunk.css"
+  },
+  {
+    "revision": "c90c083ec67a31d15529",
+    "url": "/handwrite-web/static/js/2.34f2686d.chunk.js"
+  },
+  {
+    "revision": "54b0d79c10e8c4d1ebefe9c44a44eab3",
+    "url": "/handwrite-web/static/media/logo.54b0d79c.svg"
+  },
+  {
+    "revision": "3bbe02075c077ce32b41d1e75002f21e",
+    "url": "/handwrite-web/index.html"
+  }
+];
